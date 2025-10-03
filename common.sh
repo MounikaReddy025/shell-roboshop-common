13ad1020bfd263e53741192683d3bc1bccecdaa0
mounika <info@joindevops.com>
2025-10-03 17:07:32 +0530

    session-06

--- common.sh
+++ common.sh
@@ -30,6 +30,50 @@ VALIDATE(){ #functions receive inputs through args just like shell script args
     fi
 }
 
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Disabling NodeJS"
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "Enabling NodeJS"
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Installing NodeJS"
+
+    npm install &>>$LOG_FILE
+    VALIDATE $? "Install dependencies"
+
+}
+
+app_setup(){
+    mkdir -p /app 
+    VALIDATE $? "Creating app directory" 
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "Downloading $app_name application"
+
+    cd /app 
+    VALIDATE $? "Changing to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing exixiting code"
+
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "unzip $app_name"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "Copy systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $app_name &>>$LOG_FILE
+    VALIDATE $? "Enable $app_name"
+
+}
+app_restart(){
+    systemctl restart $app_name
+    VALIDATE $? "Restarted $app_name"
+
+}
 print_total_time(){
     END_TIME=$(date +%s)
     TOTAL_TIME=$(( $END_TIME - $START_TIME))
