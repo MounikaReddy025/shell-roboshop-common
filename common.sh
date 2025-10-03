d36ddc13dbc075e794d83045ffd879c60b54e262
mounika <info@joindevops.com>
2025-10-03 15:12:22 +0530

    session-06

---
+++ common.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+check_root(){ # checking the user is root user are not
+    if [ $USERID -ne 0 ]; then
+        echo "ERROR:: please run this script with root privelege"
+        exit 1 # failure is other than 0
+    fi
+}
+
+VALIDATE(){ #functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G is SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+print_total_time(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( $END_TIME - $START_TIME))
+    echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
+}
+
+
+
