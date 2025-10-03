ff0fcbe18ae1dd8e97e5f8bb78a8e4c7b8e1baf9
mounika <info@joindevops.com>
2025-10-03 17:13:34 +0530

    session-06

--- common.sh
+++ common.sh
@@ -10,6 +10,8 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 START_TIME=$(date +%s)
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.mouni86s.fun
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
